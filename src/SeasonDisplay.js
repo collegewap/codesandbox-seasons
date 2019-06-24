3a88fccb664c7fd640142996e06c20458d2f81d9
collegewap <collegewap@gmail.com>
2019-06-24 13:27:31 +0530

    init
    init

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -3,9 +3,14 @@ import React, { Component } from "react";
 export default class SeasonDisplay extends Component {
   render() {
     const season = getSeason(this.props.latitude, new Date().getMonth());
+    const text =
+      season === "Summer" ? "Let's Hit the beach" : "Brr.. Its Chilly";
+    const icon = season === "Summer" ? "snowflake" : "sun";
     return (
       <div>
-        {season === "Summer" ? "Let's Hit the beach" : "Brr.. Its Chilly"}
+        <i className={` ${icon} icon`} />
+        <h1>{text}</h1>
+        <i className={` ${icon} icon`} />
       </div>
     );
   }
