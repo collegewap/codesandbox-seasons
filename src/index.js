b3d49fdf4b863f907ca315035eb79e01c88d1aca
collegewap <collegewap@gmail.com>
2019-06-29 10:57:50 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom";
 import React, { Component } from "react";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends Component {
   state = { latitude: null, errorMessage: "" };
@@ -10,14 +11,18 @@ class App extends Component {
       err => this.setState({ errorMessage: err.message })
     );
   }
-  render() {
+
+  renderContent() {
     if (this.state.latitude != null && this.state.errorMessage === "") {
       return <SeasonDisplay latitude={this.state.latitude} />;
     } else if (this.state.latitude == null && this.state.errorMessage === "") {
-      return <div>Loading..</div>;
+      return <Spinner message="Please accept the location request" />;
     } else {
       return <div>{this.state.errorMessage}</div>;
     }
+  }
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
